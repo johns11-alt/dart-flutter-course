7fe571576d07c7d0b8f4530470540195841661ce
johns <johns11.alt@gmail.com>
2025-06-20 12:01:22 +0300

    69 - add questions page

--- quiz_app/lib/questions_screen.dart
+++ quiz_app/lib/questions_screen.dart
@@ -12,6 +12,22 @@ class QuestionsScreen extends StatefulWidget {
 class _QuestionsScreenState extends State<QuestionsScreen> {
   @override
   Widget build(context) {
-    return Text('QuestionsScreen');
+    return SizedBox(
+      width: double.infinity, 
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+        const Text('The question...'),
+        const SizedBox(height: 30),
+        ElevatedButton(onPressed: () {}, 
+        child: Text('Answer 1')),
+        ElevatedButton(onPressed: () {}, 
+        child: Text('Answer 2')),
+        ElevatedButton(onPressed: () {}, 
+        child: Text('Answer 3')),
+        ElevatedButton(onPressed: () {}, 
+        child: Text('Answer 4')),
+      ]),
+    );
   }
 }
