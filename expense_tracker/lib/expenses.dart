70225ac939f07a83e745eea04bcd52af1a8056cc
johns <johns11.alt@gmail.com>
2025-06-23 13:23:05 +0300

    102 - add date through constructor

--- expense_tracker/lib/expenses.dart
+++ expense_tracker/lib/expenses.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:expense_tracker/models/expense.dart';
 
 class Expenses extends StatefulWidget {
   const Expenses({super.key});
@@ -10,14 +11,25 @@ class Expenses extends StatefulWidget {
 }
 
 class _ExpensesState extends State<Expenses> {
+  final List<Expense> _registeredExpenses = [
+    Expense(
+      title: 'Flutter Course',
+      amount: 19.99,
+      date: DateTime.now(),
+      category: Category.work,
+    ),
+    Expense(
+      title: 'Cinema',
+      amount: 15.69,
+      date: DateTime.now(),
+      category: Category.leisure,
+    ),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: 
-      Column(children: [
-        Text('The chart'),
-        Text('Expenses list'),
-      ],)
+      body: Column(children: [Text('The chart'), Text('Expenses list')]),
     );
   }
 }
