bc94e81af3a3ae53884cbc502da3fd802dd8da02
johns <johns11.alt@gmail.com>
2025-06-24 13:46:44 +0300

    130 - add dark mode

--- expense_tracker/lib/main.dart
+++ expense_tracker/lib/main.dart
@@ -6,10 +6,27 @@ var kColorScheme = ColorScheme.fromSeed(
   seedColor: Color.fromARGB(255, 96, 59, 181),
 );
 
+var kDarkColorScheme = ColorScheme.fromSeed(
+  seedColor: Color.fromARGB(255, 5, 99, 125),
+  brightness: Brightness.dark
+);
+
 void main() {
   runApp(
     MaterialApp(
-      // Enable Material 3 design
+      darkTheme: ThemeData.dark().copyWith(
+        colorScheme: kDarkColorScheme,
+        cardTheme: CardThemeData(
+          color: kDarkColorScheme.secondaryContainer,
+          margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+        ),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+            backgroundColor: kDarkColorScheme.primaryContainer,
+            foregroundColor: kDarkColorScheme.onPrimaryContainer
+          ),
+        ),
+      ),
       theme: ThemeData().copyWith(
         colorScheme: kColorScheme,
         // useMaterial3: true,
@@ -20,10 +37,7 @@ void main() {
         ),
         cardTheme: CardThemeData(
           color: kColorScheme.secondaryContainer,
-          margin: const EdgeInsets.symmetric(
-            horizontal: 16,
-            vertical: 8,
-          ),
+          margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
         ),
         elevatedButtonTheme: ElevatedButtonThemeData(
           style: ElevatedButton.styleFrom(
@@ -38,6 +52,8 @@ void main() {
           ),
         ),
       ),
+      // themeMode: ThemeMode.system,    //!!!this is the default
+      // themeMode: ThemeMode.dark,      //hard-code apply the dark theme
       home: const Expenses(),
     ),
   );
