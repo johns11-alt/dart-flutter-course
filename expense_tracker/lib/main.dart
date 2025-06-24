8384921c24a508b8ded753b0454e1a0faec8482d
johns <johns11.alt@gmail.com>
2025-06-24 12:15:07 +0300

    125 - theming intro

--- expense_tracker/lib/main.dart
+++ expense_tracker/lib/main.dart
@@ -4,6 +4,7 @@ import 'package:expense_tracker/widgets/expenses.dart';
 void main() {
     runApp(MaterialApp(
       // theme: ThemeData(useMaterial3: true),
+      theme: ThemeData().copyWith(scaffoldBackgroundColor: Color.fromARGB(255, 220, 189, 252)),
       home: Expenses(),
     )
   );
