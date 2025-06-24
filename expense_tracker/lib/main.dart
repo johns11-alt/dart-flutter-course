6c67bdab2c706df0058e4ce287b06c71554ed471
johns <johns11.alt@gmail.com>
2025-06-24 12:29:54 +0300

    126 - theming colorScheme

--- expense_tracker/lib/main.dart
+++ expense_tracker/lib/main.dart
@@ -1,10 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:expense_tracker/widgets/expenses.dart';
 
+var kColorScheme = ColorScheme.fromSeed(seedColor: Color.fromARGB(255, 96, 59, 181));
+
 void main() {
     runApp(MaterialApp(
       // theme: ThemeData(useMaterial3: true),
-      theme: ThemeData().copyWith(scaffoldBackgroundColor: Color.fromARGB(255, 220, 189, 252)),
+      theme: ThemeData().copyWith(
+        // scaffoldBackgroundColor: Color.fromARGB(255, 220, 189, 252),
+        // elevatedButtonTheme: ElevatedButtonThemeData(),
+        colorScheme: kColorScheme,
+        appBarTheme: const AppBarTheme().copyWith(
+          backgroundColor: kColorScheme.onPrimaryContainer,
+          foregroundColor: kColorScheme.primaryContainer,
+        )
+        ),
       home: Expenses(),
     )
   );
