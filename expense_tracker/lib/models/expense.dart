aa3df8dcdd8dcc99334d064c95c5c1385f57ab0b
johns <johns11.alt@gmail.com>
2025-06-24 14:07:12 +0300

    132 - alt constructors to filter categories

--- expense_tracker/lib/models/expense.dart
+++ expense_tracker/lib/models/expense.dart
@@ -33,22 +33,25 @@ class Expense {
   }
 }
 
+// CHARTS CLASS
 class ExpenseBucket {
-  const ExpenseBucket({
-    required this.category,
-    required this.expenses
-  });
+  const ExpenseBucket({required this.category, required this.expenses});
 
-final Category category;
-final List<Expense> expenses;
+  ExpenseBucket.forCategory(
+    List<Expense> allExpenses, this.category
+    ) : expenses = allExpenses.where(
+    (expense) => expense.category == category
+    ).toList();
 
-double get totalExpenses {
-  double sum = 0;
+  final Category category;
+  final List<Expense> expenses;
 
-  for (final expense in expenses) {
-    sum += expense.amount;
+  double get totalExpenses {
+    double sum = 0;
+
+    for (final expense in expenses) {
+      sum += expense.amount;
+    }
+    return sum;
   }
-  return sum;
-}
-
 }
