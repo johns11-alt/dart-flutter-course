ac265c592ae5f41ea4b3bc41b4776e83ce167f15
johns <johns11.alt@gmail.com>
2025-06-24 12:49:49 +0300

    127 - more expenses app theming

--- expense_tracker/lib/main.dart
+++ expense_tracker/lib/main.dart
@@ -1,21 +1,44 @@
 import 'package:flutter/material.dart';
 import 'package:expense_tracker/widgets/expenses.dart';
 
-var kColorScheme = ColorScheme.fromSeed(seedColor: Color.fromARGB(255, 96, 59, 181));
+// Define a custom color scheme
+var kColorScheme = ColorScheme.fromSeed(
+  seedColor: Color.fromARGB(255, 96, 59, 181),
+);
 
 void main() {
-    runApp(MaterialApp(
-      // theme: ThemeData(useMaterial3: true),
+  runApp(
+    MaterialApp(
+      // Enable Material 3 design
       theme: ThemeData().copyWith(
-        // scaffoldBackgroundColor: Color.fromARGB(255, 220, 189, 252),
-        // elevatedButtonTheme: ElevatedButtonThemeData(),
         colorScheme: kColorScheme,
-        appBarTheme: const AppBarTheme().copyWith(
+        // useMaterial3: true,
+        appBarTheme: AppBarTheme(
           backgroundColor: kColorScheme.onPrimaryContainer,
           foregroundColor: kColorScheme.primaryContainer,
-        )
+          centerTitle: true,
         ),
-      home: Expenses(),
-    )
+        cardTheme: CardThemeData(
+          color: kColorScheme.secondaryContainer,
+          margin: const EdgeInsets.symmetric(
+            horizontal: 16,
+            vertical: 8,
+          ),
+        ),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+            backgroundColor: kColorScheme.primaryContainer,
+          ),
+        ),
+        textTheme: ThemeData().textTheme.copyWith(
+          titleLarge: TextStyle(
+            fontWeight: FontWeight.normal,
+            color: kColorScheme.onSecondaryContainer,
+            fontSize: 14,
+          ),
+        ),
+      ),
+      home: const Expenses(),
+    ),
   );
 }
