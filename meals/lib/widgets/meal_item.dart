b0c9e4467f4d776f82e62678732ec8a978417f68
johns <johns11.alt@gmail.com>
2025-06-26 11:21:34 +0300

    166 - add Navigator to MealsDetailScree

--- meals/lib/widgets/meal_item.dart
+++ meals/lib/widgets/meal_item.dart
@@ -1,18 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:meals/screens/meal_details.dart';
 import 'package:transparent_image/transparent_image.dart';
 
 import 'package:meals/widgets/meal_item_trait.dart';
 import 'package:meals/models/meal.dart';
 
 class MealItem extends StatelessWidget {
-  const MealItem({super.key, required this.meal});
+  const MealItem({super.key, required this.meal, required this.onSelectMeal});
 
   final Meal meal;
+  final void Function(BuildContext context, Meal meal) onSelectMeal;
 
   String get complexityText {
     return meal.complexity.name[0].toUpperCase() +
         meal.complexity.name.substring(1);
   }
+
   String get affordabilityText {
     return meal.affordability.name[0].toUpperCase() +
         meal.affordability.name.substring(1);
@@ -26,7 +29,9 @@ class MealItem extends StatelessWidget {
       clipBehavior: Clip.hardEdge,
       elevation: 2,
       child: InkWell(
-        onTap: () {},
+        onTap: () {
+          onSelectMeal(context, meal);
+        },
         child: Stack(
           children: [
             FadeInImage(
@@ -72,10 +77,7 @@ class MealItem extends StatelessWidget {
                           label: '${meal.duration} min',
                         ),
                         const SizedBox(width: 12),
-                        MealItemTrait(
-                          icon: Icons.work,
-                          label: complexityText,
-                        ),
+                        MealItemTrait(icon: Icons.work, label: complexityText),
                         const SizedBox(width: 12),
                         MealItemTrait(
                           icon: Icons.attach_money,
