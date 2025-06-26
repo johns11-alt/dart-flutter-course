93ad6a643529604cccbcdba91e38cdd9fb3e5940
johns <johns11.alt@gmail.com>
2025-06-26 16:56:56 +0300

    178 - read selected data from filters - NOT WORKING

--- meals/lib/screens/tabs.dart
+++ meals/lib/screens/tabs.dart
@@ -49,12 +49,13 @@ class _TabsScreenState extends State<TabsScreen> {
 
   void _setScreen(String identifier) {
     Navigator.of(context).pop();
-    if (identifier == 'filters') {;
-      Navigator.of(context).push(
-        MaterialPageRoute(
-          builder: (ctx) => const FiltersScreen())
+    if (identifier == 'filters') {
+      ;
+      final result = Navigator.of(context).push<Map<Filter, bool>>(
+        MaterialPageRoute(builder: (ctx) => const FiltersScreen()),
       );
-    } 
+      print(result);
+    }
   }
 
   @override
