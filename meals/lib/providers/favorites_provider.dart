7b95b752acc5a0b4599008efaf1d310b6590f12d
johns <johns11.alt@gmail.com>
2025-06-27 13:01:18 +0300

    189 - set notifier method

--- meals/lib/providers/favorites_provider.dart
+++ meals/lib/providers/favorites_provider.dart
@@ -6,17 +6,20 @@ class FavoriteMealsNotifier extends StateNotifier<List<Meal>> {
 
   //always a new object in memory
 
-  void toggleMealFavoriteStatus(Meal meal) {
+  bool toggleMealFavoriteStatus(Meal meal) {
     final mealIsFavorite = state.contains(meal);
 
     if (mealIsFavorite) {
       state = state.where((m) => m.id != meal.id).toList();
+      return false;
     } else {
       state = [...state, meal];
+      return true;
     }
   }
 }
 
-final favoriteMealsProvider = StateNotifierProvider<FavoriteMealsNotifier, List<Meal>>((ref) {
+final favoriteMealsProvider =
+    StateNotifierProvider<FavoriteMealsNotifier, List<Meal>>((ref) {
   return FavoriteMealsNotifier();
 });
