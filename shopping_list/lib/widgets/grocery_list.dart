4b174ee33a537f9f900e9012728f498a8f3d443f
johns <johns11.alt@gmail.com>
2025-06-30 14:36:58 +0300

    219 - make items removable

--- shopping_list/lib/widgets/grocery_list.dart
+++ shopping_list/lib/widgets/grocery_list.dart
@@ -24,28 +24,36 @@ class _GroceryListState extends State<GroceryList> {
     });
   }
 
+  void _removeItem(GroceryItem item) {
+    setState(() {
+      _groceryItems.remove(item);
+    });
+  }
+
   final List<GroceryItem> _groceryItems = [];
 
   @override
   Widget build(BuildContext context) {
-
-    Widget content = Center(
-      child: Text('No items added yet')
-    );
+    Widget content = Center(child: Text('No items added yet'));
 
     if (_groceryItems.isNotEmpty) {
-      content = 
-      ListView.builder(
+      content = ListView.builder(
         itemCount: _groceryItems.length,
 
-        itemBuilder: (ctx, index) => ListTile(
-          title: Text(_groceryItems[index].name),
-          leading: Container(
-            width: 24,
-            height: 24,
-            color: _groceryItems[index].category.color,
+        itemBuilder: (ctx, index) => Dismissible(
+          onDismissed: (direction) {
+            _removeItem(_groceryItems[index]);
+          },
+          key: ValueKey(_groceryItems[index].id),
+          child: ListTile(
+            title: Text(_groceryItems[index].name),
+            leading: Container(
+              width: 24,
+              height: 24,
+              color: _groceryItems[index].category.color,
+            ),
+            trailing: Text(_groceryItems[index].quantity.toString()),
           ),
-          trailing: Text(_groceryItems[index].quantity.toString()),
         ),
       );
     }
@@ -55,7 +63,7 @@ class _GroceryListState extends State<GroceryList> {
         title: const Text('Your Groceries'),
         actions: [IconButton(onPressed: _addItem, icon: const Icon(Icons.add))],
       ),
-      body: content
+      body: content,
     );
   }
 }
