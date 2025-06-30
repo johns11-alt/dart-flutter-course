7fa5f79ff6c057482ae4ac9bb8c3265d352eb9a4
johns <johns11.alt@gmail.com>
2025-06-30 12:37:12 +0300

    213 - create form

--- shopping_list/lib/widgets/new_item.dart
+++ shopping_list/lib/widgets/new_item.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:shopping_list/data/categories.dart';
 import 'package:shopping_list/widgets/grocery_list.dart';
 
 class NewItem extends StatefulWidget {
@@ -21,11 +22,45 @@ class _NewItemState extends State<NewItem> {
           child: Column(
             children: [
               TextFormField(
-                maxLength: 50,
-                decoration: const InputDecoration(label: Text('Name')),
-                validator: (value) {
-                  return 'Test...';
-                },
+                  maxLength: 50,
+                  decoration: const InputDecoration(label: Text('Name')),
+                  validator: (value) {
+                    return 'Test...';
+                  },
+                ),
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: [
+                  Expanded(
+                    child: TextFormField(
+                      decoration: const InputDecoration(label: Text('Quantity')),
+                      initialValue: '1',
+                    ),
+                  ),
+                  const SizedBox(width: 8),
+                  Expanded(
+                    child: DropdownButtonFormField(
+                      items: [
+                        for (final category in categories.entries)
+                          DropdownMenuItem(
+                            value: category.value,
+                            child: Row(
+                              children: [
+                                Container(
+                                  width: 16,
+                                  height: 16,
+                                  color: category.value.color,
+                                ),
+                                const SizedBox(width: 6),
+                                Text(category.value.title),
+                              ],
+                            ),
+                          ),
+                      ],
+                      onChanged: (value) {},
+                    ),
+                  ),
+                ],
               ), //instead of TextField()
             ],
           ),
