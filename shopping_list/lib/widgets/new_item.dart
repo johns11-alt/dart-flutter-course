8525cf48f06da8f4d28633e6b67405ed037784bc
johns <johns11.alt@gmail.com>
2025-06-30 11:52:50 +0300

    211 - new item screen and functionality

---
+++ shopping_list/lib/widgets/new_item.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:shopping_list/widgets/grocery_list.dart';
+
+class NewItem extends StatefulWidget {
+  const NewItem({super.key});
+
+  @override
+  State<NewItem> createState() {
+    return _NewItemState();
+  }
+}
+
+class _NewItemState extends State<NewItem> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Add a new item'),
+      ),
+      body: Padding(padding: const EdgeInsets.all(12), child: Text('The form')),
+    );
+  }
+}
