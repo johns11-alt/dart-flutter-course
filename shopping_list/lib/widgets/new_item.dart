e4223247cb682538a8fdced43431a1d5eb259e7a
johns <johns11.alt@gmail.com>
2025-06-30 16:30:40 +0300

    227 - POST request & response

--- shopping_list/lib/widgets/new_item.dart
+++ shopping_list/lib/widgets/new_item.dart
@@ -20,23 +20,32 @@ class _NewItemState extends State<NewItem> {
   var _enteredQuantity = 1;
   var _selectedCategory = categories[Categories.vegetables]!;
 
-  void _saveItem() {
+  void _saveItem() async {
     if (_formKey.currentState!.validate()) {
       _formKey.currentState!.save();
       final url = Uri.https(
         'flutter-prep-a729f-default-rtdb.firebaseio.com',
         'shopping-list.json',
       );
-      http.post(
-        url, 
-        headers: {'Content-Type': 'application/json'},
-        body: json.encode({
-          'name': _enteredName,
-          'quantity': _enteredQuantity,
-          'category': _selectedCategory.title,
-        })
-        );
-      // Navigator.of(context).pop();
+      await http
+          .post(
+            url,
+            headers: {'Content-Type': 'application/json'},
+            body: json.encode({
+              'name': _enteredName,
+              'quantity': _enteredQuantity,
+              'category': _selectedCategory.title,
+            }),
+          )
+          .then((response) {
+            print(response.body);
+            print(response.statusCode);
+          });
+
+      if (!context.mounted) {
+        return;
+      }
+      Navigator.of(context).pop();
     }
   }
 
