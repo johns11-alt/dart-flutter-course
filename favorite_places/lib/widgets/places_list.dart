2c277707fc4114a64bd80271c445bb43351e9b37
johns <johns11.alt@gmail.com>
2025-07-01 16:05:51 +0300

    244 - add place details screen

--- favorite_places/lib/widgets/places_list.dart
+++ favorite_places/lib/widgets/places_list.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:favorite_places/models/place.dart';
+import 'package:favorite_places/screens/place_detail.dart';
 
 class PlacesList extends StatelessWidget {
   const PlacesList({super.key, required this.places});
@@ -9,9 +10,14 @@ class PlacesList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     if (places.isEmpty) {
-      return Center(child: Text('No places added yet.',
-      style: Theme.of(context).textTheme.bodyLarge!.copyWith(
-            color: Theme.of(context).colorScheme.onSurface)));
+      return Center(
+        child: Text(
+          'No places added yet.',
+          style: Theme.of(context).textTheme.bodyLarge!.copyWith(
+            color: Theme.of(context).colorScheme.onSurface,
+          ),
+        ),
+      );
     }
 
     return ListView.builder(
@@ -20,9 +26,17 @@ class PlacesList extends StatelessWidget {
         title: Text(
           places[index].title,
           style: Theme.of(context).textTheme.titleMedium!.copyWith(
-            color: Theme.of(context).colorScheme.onSurface)
+            color: Theme.of(context).colorScheme.onSurface,
+          ),
+        ),
+        onTap: () {
+          Navigator.of(context).push(
+            MaterialPageRoute(
+              builder: (ctx) => PlaceDetailScreen(place: places[index]),
             ),
-        
-      ));
+          );
+        },
+      ),
+    );
   }
 }
