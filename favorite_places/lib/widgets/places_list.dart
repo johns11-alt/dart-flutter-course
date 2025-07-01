1c885222c07df70b96ba6d4013b3f79406ba5ae7
johns <johns11.alt@gmail.com>
2025-07-01 14:11:28 +0300

    240 - add places screen-add styles

--- favorite_places/lib/widgets/places_list.dart
+++ favorite_places/lib/widgets/places_list.dart
@@ -10,7 +10,7 @@ class PlacesList extends StatelessWidget {
   Widget build(BuildContext context) {
     if (places.isEmpty) {
       return Center(child: Text('No places added yet.',
-      style: Theme.of(context).textTheme.titleMedium!.copyWith(
+      style: Theme.of(context).textTheme.bodyLarge!.copyWith(
             color: Theme.of(context).colorScheme.onSurface)));
     }
 
