d470e5da02ef750f771e0b1a1cf9317017a76aa9
johns <johns11.alt@gmail.com>
2025-07-02 14:15:35 +0300

    261 - display the picked location on dynamic map

--- favorite_places/lib/screens/place_detail.dart
+++ favorite_places/lib/screens/place_detail.dart
@@ -1,5 +1,5 @@
+import 'package:favorite_places/screens/map.dart';
 import 'package:flutter/material.dart';
-
 import 'package:favorite_places/models/place.dart';
 
 class PlaceDetailScreen extends StatelessWidget {
@@ -23,46 +23,49 @@ class PlaceDetailScreen extends StatelessWidget {
       ),
       body: Stack(
         children: [
-          Image.file(
-            place.image,
-            fit: BoxFit.cover,
-            width: double.infinity,
-            height: double.infinity
-          ),
+          Image.file(place.image,
+              fit: BoxFit.cover,
+              width: double.infinity,
+              height: double.infinity),
           Positioned(
             bottom: 0,
             left: 0,
             right: 0,
-            child: Column(children: [
-              CircleAvatar(
-                radius: 70,
-                backgroundImage: NetworkImage(locationImage),
-              ),
-              Container(
-                alignment: Alignment.center,
-                padding: const EdgeInsets.symmetric(
-                  horizontal: 24,
-                  vertical: 16
+            child: Column(
+              children: [
+                GestureDetector(
+                  onTap: () {
+                    Navigator.of(context)
+                        .push(MaterialPageRoute(builder: (ctx) => MapScreen(
+                          location: place.location,
+                          isSelecting: false,
+                        )));
+                  },
+                  child: CircleAvatar(
+                    radius: 70,
+                    backgroundImage: NetworkImage(locationImage),
                   ),
-                decoration: const BoxDecoration(
-                  gradient: LinearGradient(colors: [
+                ),
+                Container(
+                  alignment: Alignment.center,
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 24, vertical: 16),
+                  decoration: const BoxDecoration(
+                      gradient: LinearGradient(colors: [
                     Colors.transparent,
                     Colors.black54,
-                  ],
-                  begin: Alignment.topCenter,
-                  end: Alignment.bottomCenter
-                  )
-                ),
-                child: Text(
-                  place.location.address,
-                  textAlign: TextAlign.center,
-                  style: Theme.of(context).textTheme.titleLarge!.copyWith(
-                color: Theme.of(context).colorScheme.onSurface,
-              ),
-                ),
-              )
-            ],),
+                  ], begin: Alignment.topCenter, end: Alignment.bottomCenter)),
+                  child: Text(
+                    place.location.address,
+                    textAlign: TextAlign.center,
+                    style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                          color: Theme.of(context).colorScheme.onSurface,
+                        ),
+                  ),
+                )
+              ],
             ),
+          ),
         ],
       ),
     );
