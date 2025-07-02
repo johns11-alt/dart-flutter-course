b19f41c74c217de3ab805d11794730b32d1500d4
johns <johns11.alt@gmail.com>
2025-07-02 13:10:29 +0300

    258 - output location data

--- favorite_places/lib/widgets/places_list.dart
+++ favorite_places/lib/widgets/places_list.dart
@@ -31,9 +31,15 @@ class PlacesList extends StatelessWidget {
         title: Text(
           places[index].title,
           style: Theme.of(context).textTheme.titleMedium!.copyWith(
-                color: Theme.of(context).colorScheme.onBackground,
+                color: Theme.of(context).colorScheme.onSurface,
               ),
         ),
+        subtitle: Text(
+          places[index].location.address,
+          style: Theme.of(context).textTheme.bodySmall!.copyWith(
+                color: Theme.of(context).colorScheme.onSurface,
+              ),
+          ),
         onTap: () {
           Navigator.of(context).push(
             MaterialPageRoute(
