07fae9e13297d0effa8081a309fecf595e15e3a6
johns <johns11.alt@gmail.com>
2025-07-07 12:40:32 +0300

    setting styles

--- test/lib/screens/first_screen.dart
+++ test/lib/screens/first_screen.dart
@@ -39,38 +39,62 @@ class _FirstScreenState extends State<FirstScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Appbar Title'),
+         backgroundColor: Colors.black,
+        foregroundColor: Colors.white,
+        title: const Text('Notes App'),
       ),
-      body: Card(
-        margin: const EdgeInsets.all(20),
-        child: SingleChildScrollView(
-          child: Padding(
-            padding: const EdgeInsets.all(16),
-            child: Form(
-              key: _formKey,
-              child: Column(
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  TextFormField(
-                    controller: _passwordController,
-                    decoration: const InputDecoration(labelText: 'Password'),
-                    obscureText: true,
+      body: Container(
+         color: Colors.white,
+        child: Center(
+          child: SingleChildScrollView(
+            padding: const EdgeInsets.all(24),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Image.asset(
+                  'assets/images/logo.jpg', // Replace with your image path
+                  height: 300,
+                ),
+                const SizedBox(height: 24),
+                Form(
+                  key: _formKey,
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      TextFormField(
+                        controller: _passwordController,
+                        obscureText: true,
+                         decoration: InputDecoration(
+                          hintText: 'Password',               // placeholder text inside the field
+                          enabledBorder: OutlineInputBorder(
+                            borderSide: BorderSide(color: Colors.black),
+                            borderRadius: BorderRadius.circular(4),
+                          ),
+                          focusedBorder: OutlineInputBorder(
+                            borderSide: BorderSide(color: Colors.black, width: 2),
+                            borderRadius: BorderRadius.circular(5),
+                          ),
+                         ),
+                      ),
+                      const SizedBox(height: 12),
+                      ElevatedButton(
+                        onPressed: _submit,
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: Colors.black,
+                          foregroundColor: Colors.white
+                        ),
+                        child: const Text('Είσοδος'),
+                      ),
+                    ],
                   ),
-                  const SizedBox(height: 12),
-                  ElevatedButton(
-                    onPressed: _submit,
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor:
-                          Theme.of(context).colorScheme.primaryContainer,
-                    ),
-                    child: const Text('Login'),
-                  ),
-                ],
-              ),
+                ),
+              ],
             ),
           ),
         ),
-      ),
+      )
+
     );
   }
 }
--- test/lib/screens/second_screen.dart
+++ test/lib/screens/second_screen.dart
@@ -83,106 +83,113 @@ class _SecondScreenState extends State<SecondScreen> {
           ),
         ],
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            const Text(
-              'Προσθήκη σημείωσης:',
-              style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-            ),
-            const SizedBox(height: 12),
-            Row(
-              children: [
-                Expanded(
-                  child: TextField(
-                    controller: _noteController,
-                    decoration: const InputDecoration(
-                      border: OutlineInputBorder(),
+      body: Container(
+        color: Colors.white,
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Center(
+                child: const Text(
+                  'Προσθήκη σημείωσης:',
+                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                ),
+              ),
+              const SizedBox(height: 12),
+              Row(
+                children: [
+                  Expanded(
+                    child: TextField(
+                      controller: _noteController,
+                      decoration: const InputDecoration(
+                        border: OutlineInputBorder(),
+                      ),
                     ),
                   ),
-                ),
-                const SizedBox(width: 10),
-                CircleAvatar(
-                  backgroundColor: Colors.green,
-                  child: IconButton(
-                    icon: const Icon(Icons.add, color: Colors.white),
-                    onPressed: _addNote,
-                  ),
-                ),
-                const SizedBox(width: 10),
-                CircleAvatar(
-                  backgroundColor: const Color.fromARGB(255, 224, 119, 38),
-                  child: TextButton(
-                    onPressed: _getData,
-                    child: const Text(
-                      'api',
-                      style: TextStyle(color: Colors.white),
+                  const SizedBox(width: 10),
+                  CircleAvatar(
+                    backgroundColor: Colors.green,
+                    child: IconButton(
+                      icon: const Icon(Icons.add, color: Colors.white),
+                      onPressed: _addNote,
                     ),
                   ),
-                ),
-              ],
-            ),
-            const SizedBox(height: 20),
-            const Text('your jobs:', style: TextStyle(fontSize: 16)),
-            const SizedBox(height: 10),
-            Expanded(
-              child: notes.isEmpty
-                  ? const Center(child: Text('No notes yet!'))
-                  : ListView.builder(
-                      itemCount: notes.length,
-                      itemBuilder: (ctx, index) {
-                        final note = notes[index];
-
-                        if (note['type'] == 'api') {
-                          return Padding(
-                            padding: const EdgeInsets.symmetric(vertical: 4),
-                            child: Card(
-                              child: Column(
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                children: [
-                                  Image.network(
-                                    note['image']!,
-                                    width: double.infinity,
-                                    height: 200,
-                                    fit: BoxFit.cover,
-                                  ),
-                                  Padding(
-                                    padding: const EdgeInsets.all(8.0),
-                                    child: Text(note['fact']!),
-                                  ),
-                                  Align(
-                                    alignment: Alignment.centerRight,
-                                    child: IconButton(
-                                      icon: const Icon(Icons.delete),
-                                      color: Colors.red,
-                                      onPressed: () => _removeNote(index),
+                  const SizedBox(width: 10),
+                  CircleAvatar(
+                    backgroundColor: const Color.fromARGB(255, 224, 119, 38),
+                    child: TextButton(
+                      onPressed: _getData,
+                      child: const Text(
+                        'api',
+                        style: TextStyle(color: Colors.white),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              const SizedBox(height: 20),
+              // const Text('your jobs:', style: TextStyle(fontSize: 16)),
+              const SizedBox(height: 10),
+              Expanded(
+                child: notes.isEmpty
+                    ? const Center(child: Text('No notes yet!'))
+                    : ListView.builder(
+                        
+                        itemCount: notes.length,
+                        itemBuilder: (ctx, index) {
+                          final note = notes[index];
+        
+                          if (note['type'] == 'api') {
+                            return Padding(
+                              padding: const EdgeInsets.symmetric(vertical: 4),
+                              child: Card(
+                                child: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Image.network(
+                                      note['image']!,
+                                      width: double.infinity,
+                                      height: 200,
+                                      fit: BoxFit.cover,
                                     ),
-                                  ),
-                                ],
-                              ),
-                            ),
-                          );
-                        } else {
-                          return Padding(
-                            padding: const EdgeInsets.symmetric(vertical: 4),
-                            child: Card(
-                              child: ListTile(
-                                title: Text(note['note']!),
-                                trailing: IconButton(
-                                  icon: const Icon(Icons.delete),
-                                  color: Colors.red,
-                                  onPressed: () => _removeNote(index),
+                                    Padding(
+                                      padding: const EdgeInsets.all(8.0),
+                                      child: Text(note['fact']!),
+                                    ),
+                                    Align(
+                                      alignment: Alignment.centerRight,
+                                      child: IconButton(
+                                        icon: const Icon(Icons.delete),
+                                        color: Colors.red,
+                                        onPressed: () => _removeNote(index),
+                                      ),
+                                    ),
+                                  ],
                                 ),
                               ),
-                            ),
-                          );
-                        }
-                      },
-                    ),
-            ),
-          ],
+                            );
+                          } else {
+                            return Padding(
+                              padding: const EdgeInsets.symmetric(vertical: 4),
+                              child: Card(
+                                child: ListTile(
+                                  title: Text(note['note']!),
+                                  trailing: IconButton(
+                                    icon: const Icon(Icons.delete),
+                                    color: Colors.red,
+                                    onPressed: () => _removeNote(index),
+                                  ),
+                                ),
+                              ),
+                            );
+                          }
+                        },
+                      ),
+              ),
+            ],
+          ),
         ),
       ),
     );
